0cc8a708009f33c236ba6f9e147fa2762092f73a
ashfaqFullstack <ashfaqahmadfullstack@gmail.com>
2026-09-18 17:14:31 +0500

    updated frontend url

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // cors origin security
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.1.11:3000'],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(cookieParser());
